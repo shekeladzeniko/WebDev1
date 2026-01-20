647ca675008373c18aa56f32c45ae1dae5f3ffe5
shekeladzeniko <shekeladzenikoloz95@gmail.com>
2026-01-20 23:04:47 +0400

    created registr

---
+++ javascript/registra.js
@@ -0,0 +1,7 @@
+const name = prompt("saxeli?");
+const surname = prompt("gvari?"); 
+const age = prompt("asaki?"); 
+const username = prompt("username?"); 
+const pass = prompt("password?");
+
+alert("saxeli: " + name + " gvari: " + surname + " asaki: " + age + " username: " + username + " password: " + pass);

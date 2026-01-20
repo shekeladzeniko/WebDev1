b333b2cb3e7f60172a6f08600d82489c3a65e860
shekeladzeniko <shekeladzenikoloz95@gmail.com>
2026-01-20 19:57:30 +0400

    added js

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+console.warn("this is warning");
+console.error("this is error");
+
+alert("hello!");
+
+const myAge = prompt("how old are you?"); 
+
+const a = "Niko"
+console.log(a);
+
+// ctrl + /

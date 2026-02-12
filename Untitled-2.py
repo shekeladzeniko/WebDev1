e9e4017f124f8d39017f251cb213b593aad8a1ad
nikoloz11 <>
2026-02-12 18:34:28 +0400

    Create Untitled-2.py

---
+++ Untitled-2.py
@@ -0,0 +1,11 @@
+def duplicateZeros(arr):
+    result = []
+    for i in arr:
+        if  len(result) == len(arr):
+            break
+            result.append(n)
+            if n == 0: 
+             result.append(n) 
+    print(result)
+
+duplicateZeros([1,0,0,2,3,0,4,5]) 

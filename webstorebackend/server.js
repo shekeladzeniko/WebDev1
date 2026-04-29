2c51273ad656fe613306540c49a30b08a3bc2922
shekeladzeniko <shekeladzenikoloz95@gmail.com>
2026-04-29 23:03:40 +0400

    fg

--- webstorebackend/server.js
+++ webstorebackend/server.js
@@ -4,6 +4,15 @@ const app = express();
 
 const users = ["saba", "niko", "gio", "luka"];
 
+const products = [
+      {id: 1, name: "mouse", price: 30},
+      {id: 2, name: "keyboard", price: 50},
+      {id: 3, name: "monitor", price: 100},
+      {id: 4, name: "camera", price: 40},
+]
+
+
+
 app.get("/users", (req, res) => {
       res.send(users);
 });
@@ -25,6 +34,27 @@ app.post("/users", (req, res) => {
       console.log(req.body);
       res.send("welcome to the post method");
 });
+
+
+
+app.get("/products", (req, res) => {
+      res.send(products);
+})
+
+app.get("/products/:id", (req, res) => {
+      console.log(req.params.id);
+      const parsedid = parseInt(req.params.id);
+
+      if(isNaN(parsedid)){
+            return res.status(400).send("value should be number");
+      }else if(parsedid >= products.length){
+            return res.status(400).send("value should be smaller");
+      }
+
+      res.status(200).send(products[parsedid]);
+})
+
+
 
 app.listen(3000,() => {
       console.log('app is listening ar http://localhost:3000');

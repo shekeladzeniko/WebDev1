97535f3d9061657a7d9e2ff8d96f9f9b810f8108
shekeladzeniko <shekeladzenikoloz95@gmail.com>
2026-06-11 14:39:05 +0400

    cvb

--- expressMongoDB/back/routes/productRoute.js
+++ expressMongoDB/back/routes/productRoute.js
@@ -4,7 +4,7 @@ import { Product } from "../models/products.js"
 const router = express.Router();
 
 
-router.post("/", async (req, res) => {
+router.post("/postproduct", async (req, res) => {
 
       try
       {
@@ -16,7 +16,7 @@ router.post("/", async (req, res) => {
 
       await product.save();
       res.send("product added")
-      } catch (e) {
+      } catch (error) {
             res.status(400).send(error.message);
       }
 
@@ -24,16 +24,49 @@ router.post("/", async (req, res) => {
       res.send("success");
 })
 
-router.get("/getProduct", (req, res) => {
+router.get("/getproduct", async (req, res) =>{
+      try{
+            const products = await Product.find({
+                  category: "Electronics"
+            });
 
-   
-})
+            res.send(products);
+      } catch (error) {
+            res.status(400).send(error.message);
+      }
+});
 
 router.put("/putProduct", async (req, res) => {
+       try{
+            const updatedProduct = await Product.findOneAndUpdate(
+                  {
+                        name: "Smart Watch"
+                  },
+                  {
+                        price: 150
+                  },
+                  {
+                        new: true
+                  }
+      );
+            
+            res.send(updatedProduct);
+      } catch (error) {
+            res.status(400).send(error.message);
+      }
 
 })
 
 router.delete("/deleteProduct", async (req, res) => {
+       try{
+            const deletedProduct = await Product.findOneAndDelete({
+                  name: "Smart Watch"
+            });
+
+            res.send("Product deleted successfully");
+      } catch (error) {
+            res.status(400).send(error.message);
+      }
 
 })
 
--- expressMongoDB/back/routes/userRoute.js
+++ expressMongoDB/back/routes/userRoute.js
@@ -3,22 +3,67 @@ import { User } from "../models/users.js"
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
-      const user = new User({
+router.post("/postuser", (req, res) => {
+      try{
+            const user = new User({
             username: "gio",
             password: "gio123"
       })
 
-      user.save();
-
+       user.save();
       res.send("success");
+      
+      } catch(error){
+            res.status(400).send(error.message);
+      }
 });
 
-router.get("/", async (req, res) =>{
+
+router.get("/getuser", async (req, res) =>{
       try{
-            const users = await User.find();
+            const users = await User.find({
+                  name: "gio"
+            });
+
             res.send(users);
-      } catch (error) {}
+      } catch (error) {
+            res.status(400).send(error.message);
+      }
 });
+
+
+router.put("/putuser", async (req, res) => {
+       try{
+            const users = await User.findOneAndUpdate(
+                  {
+                        name: "gio"
+                  },
+                  {
+                        password: "giogio"
+                  },
+                  {
+                        new: true
+                  }
+            );
+
+            res.send(users);
+      } catch (error) {
+            res.status(400).send(error.message);
+      }
+
+})
+
+router.delete("/deleteuser", async (req, res) => {
+       try{
+            const deletedUser = await User.findOneAndDelete({
+                  name: "gio"
+            });
+
+            res.send("User deleted successfully");
+      } catch (error) {
+            res.status(400).send(error.message);
+      }
+
+})
 
 export default router;
